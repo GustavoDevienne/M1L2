7737e11e46be8d8f5aa3d1aec1a3dc78f0c6c0a8
GustavoDevienne <gustavo.devienne@gmail.com>
2026-06-13 11:20:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
-import random
-elementos = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-q1 = int(input("qual será o comprimento da sua senha? "))
-senha = ""
+import discord
+from bot_logic import gen_pass
+from discord.ext import commands
 
 
-for i in range(q1):
-    senha+= random.choice(elementos)
+# A variável intents armazena as permissões do bot
+intents = discord.Intents.default()
+# Ativar a permissão para ler o conteúdo das mensagens
+intents.message_content = True
+# Criar um bot e passar as permissões
+bot = commands.Bot(command_prefix='/', intents=intents)
 
-print(senha)
 
+@bot.command
+#comando hello
+async def hello(ctx):
+    await ctx.send("Hello")
+
+#comando bye
+async def Bye(ctx):
+    await ctx.send("Bye")
+
+async def gen_pass(ctx):
+    await ctx.send(gen_pass(10))
+
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hello!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    else:
+        await message.channel.send(message.content)
+
+bot.run("k")
